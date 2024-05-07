4c0f77138dead10a47f37d6a8724f5a59d1e3e83
Andrea Hohnsen <a.hohnsen@mail.de>
2024-05-07 09:28:13 +0200

    Add color and emoji to nav bar

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerText = document.querySelector('[data-js="answer-text"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("card__answer--active");
+
+  const answerButtonText = answerButton.textContent.trim();
+
+  answerButton.textContent =
+    answerButtonText === "Show answer" ? "Hide answer" : "Show answer";
+});
